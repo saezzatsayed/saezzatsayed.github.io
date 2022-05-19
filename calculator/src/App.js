26e3cf7d9180d7f33fba8428ea5ac65fba682feb
saezzatsayed <78116229+saezzatsayed@users.noreply.github.com>
2022-05-19 11:47:42 -0600

    Fixing eval

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -18,9 +18,48 @@ const App = () => {
     setview(stuff)
   } 
 
-  const Equal = (view) => {
-    setview(eval(view))
+  const Equal = (viewScreen) => {
+    
+    var input = viewScreen.toString();
+    var sign, result;
+    
 
+    if(input.includes("+")){
+      sign = "+"
+    }
+    else if(input.includes("-")){
+      sign = "-"
+    }
+    else if(input.includes("*")){
+      sign = "*"
+    }
+    else if(input.includes("/")){
+      sign = "/"
+    }
+
+    var equation = input.split("+" || "-" || "*" || "/")
+    
+    const equLen = equation.length;
+    
+    if(equLen > "2"){
+      alert(`Too many operatore, Please enter only 
+      two variables seperated by one operator.`)
+    }
+
+    if(sign === '+'){
+      result = Number(equation[0]) + Number(equation[1]);
+    }
+    else if(sign === '-'){
+      result = Number(equation[0]) - Number(equation[1]);
+    }
+    else if(sign === '*'){
+      result = Number(equation[0]) * Number(equation[1]);
+    }
+    else if(sign === '/'){
+      result = Number(equation[0]) / Number(equation[1]);
+    }
+
+    setview(result)
   }
 
   const ClearScreen = () => {
@@ -38,16 +77,16 @@ const App = () => {
       <Container>
       <div className="screen" data-testid="Screen">{ view || "0" }</div>
         <BtnContainer>
-        <div className="buttons" onClick={() => {setview(view+1)}} value='1'>1</div>
-        <div className="buttons"onClick={() => {setview(view+2)}} value='2'>2</div>
-        <div className="buttons" onClick={() => {setview(view+3)}} value='3'>3</div>
-        <div className="buttons" onClick={() => {setview(view+4)}} value='4'>4</div>
-        <div className="buttons" onClick={() => {setview(view+5)}} value='5'>5</div>
-        <div className="buttons" onClick={() => {setview(view+6)}} value='6'>6</div>
-        <div className="buttons" onClick={() => {setview(view+7)}} value='7'>7</div>
-        <div className="buttons" onClick={() => {setview(view+8)}} value='8'>8</div>
-        <div className="buttons" onClick={() => {setview(view+9)}} value='9'>9</div>
-        <div className="buttons" onClick={() => {setview(view+0)}} id="zero" value='0'>0</div>
+        <div className="buttons" onClick={() => {if(view === 0){setview("1")} else{setview(view+"1")}}} value='1'>1</div>
+        <div className="buttons"onClick={() => {if(view === 0){setview("2")} else{setview(view+"2")}}} value='2'>2</div>
+        <div className="buttons" onClick={() => {if(view === 0){setview("3")} else{setview(view+"3")}}} value='3'>3</div>
+        <div className="buttons" onClick={() => {if(view === 0){setview("4")} else{setview(view+"4")}}} value='4'>4</div>
+        <div className="buttons" onClick={() => {if(view === 0){setview("5")} else{setview(view+"5")}}} value='5'>5</div>
+        <div className="buttons" onClick={() => {if(view === 0){setview("6")} else{setview(view+"6")}}} value='6'>6</div>
+        <div className="buttons" onClick={() => {if(view === 0){setview("7")} else{setview(view+"7")}}} value='7'>7</div>
+        <div className="buttons" onClick={() => {if(view === 0){setview("8")} else{setview(view+"8")}}} value='8'>8</div>
+        <div className="buttons" onClick={() => {if(view === 0){setview("9")} else{setview(view+"9")}}} value='9'>9</div>
+        <div className="buttons" onClick={() => {if(view === 0){setview("0")} else{setview(view+"0")}}} id="zero" value='0'>0</div>
         <div className="buttons" onClick={() => {setview(view+".")}} value='.'>.</div>
         <div className="buttonO" onClick={() => {setview(view+"-")}} value='-'>-</div>
         <div className="buttonO" onClick={() => {setview(view+"+")}} value='+'>+</div>
